f29172ff004acc7e68294111391e190a3d20e707
adrianObel <obeladrian@gmail.com>
2014-02-17 00:37:30 -0430

    show questions by index

--- app/js/services.js
+++ app/js/services.js
@@ -3,21 +3,20 @@
 angular.module('trivia.services', [])
 	
 	.factory('triviaGame', ['$http', function($http) {
-		var questions = []
-			, triviaGame
 
 		$http.get('questions/questions.json')
 			.success(function(data, status) {
-				questions = data
-				triviaGame.currentQuestion = questions[0]
+				triviaGame.questions = data
+				triviaGame.currentQuestion = triviaGame.questions[triviaGame.questionIndex]
 			})
 
-	  triviaGame = {
+		var triviaGame = {
 			groups: function(numGroups) {
 				return numGroups === undefined ?
 					triviaGame.numGroups :
 					triviaGame.numGroups = numGroups
 			}
+		,	questionIndex: 0
 		}
 		return triviaGame
 	}])
